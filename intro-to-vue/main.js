b985fa2931bc4ab50f695edb10c4871374dcc6d4
Saul Maldonado <samaldonado4@gmail.com>
2020-04-18 07:50:01 -0500

    Added class and style bindings to stock <p> tag and variant color blocks. Added computed values that return product title, selected product image and selected product stock.

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -1,36 +1,55 @@
 const app = new Vue({
     el: '#app',
     data: {
-        product: 'socks',
+        product: 'Socks',
         description: 'Vue Socks.',
-        image: './assets/vmSocks-green.jpg',
+        
+        selectedVariant: 0,
         google:'https://www.google.com',
-        inventory: 100,
+        inventory: 0,
         onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        brand: "Vue Mastery",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral',],
         variants:[
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './assets/vmSocks-green.jpg'
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0,
             }
         ],
-        sizes:["small", "medium", "large"],
+        sizes:["small", "medium", "large",],
         cart: 0,
-
-
     },
+
     methods: {
-        addToCart: function (){
+        addToCart() {
             this.cart += 1;
         },
-        updateProduct: function (variantImage){
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
+        },
+        removeFromCart() {
+            if(this.cart > 0) {
+                this.cart -= 1;
+            }
         }
-    }
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        stock(){
+           return this.variants[this.selectedVariant].variantQuantity;
+        },
+    },
 });
