b99d36e9fbedb5faa58d5d84ed09895df1c7054d
Saul Maldonado <samaldonado4@gmail.com>
2020-04-18 05:18:30 -0500

    Added in stock field, current stock field, details list, colors list, sizes list and add to cart button with cart counter. mouseover event handler was also added to color that changes the main image.

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -1,7 +1,36 @@
 const app = new Vue({
     el: '#app',
     data: {
-        product: 'Boots',
-        description: 'These boots are green.'
+        product: 'socks',
+        description: 'Vue Socks.',
+        image: './assets/vmSocks-green.jpg',
+        google:'https://www.google.com',
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants:[
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        sizes:["small", "medium", "large"],
+        cart: 0,
+
+
+    },
+    methods: {
+        addToCart: function (){
+            this.cart += 1;
+        },
+        updateProduct: function (variantImage){
+            this.image = variantImage;
+        }
     }
 });
