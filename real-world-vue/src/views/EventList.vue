f7bafba509f3fc15f969134120313257dcc1172d
Saul Maldonado <samaldonado4@gmail.com>
2020-04-19 08:48:42 -0500

    Added eventcards for events, global BaseIcon component, local api server with json-server and EventService that fetches data from the local server.

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link
-    >&nbsp;|
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService';
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(err => {
+        console.log('There was an error: ', err.response);
+      });
+  },
+};
+</script>
